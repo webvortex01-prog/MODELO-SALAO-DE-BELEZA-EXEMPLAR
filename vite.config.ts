4c7eec66e7955fefd40f02157c3f942eafece9ee
webvortex01-prog <webvortex01@gmail.com>
2026-09-23 14:05:10 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
